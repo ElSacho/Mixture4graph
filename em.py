df47f815f09a1e2164762f125cc0d4091a600e89
alexx-frcs <alex.frcs22@gmail.com>
2023-12-14 16:15:30 +0100

    fin hierarchical

--- em.py
+++ em.py
@@ -37,6 +37,65 @@ def spectral_clustering(G, k):
     
     return tau
 
+def calculate_distance_matrix(G):
+    A= nx.adjacency_matrix(G).toarray()
+    n_vertices=G.number_of_nodes()
+    distance_matrix = np.zeros((n_vertices, n_vertices))
+
+
+    for i in range(n_vertices):
+        for j in range(i + 1, n_vertices):
+            A_copy=A.copy()
+            euclidean_matrix = (A_copy - A_copy[i,j])**2
+            distance_matrix[i, j] = distance_matrix[j, i] = np.sqrt(euclidean_matrix.sum(axis=1)-A_copy[i,j]**2)[i]
+    
+    return distance_matrix
+
+def find_closest_clusters(distance_matrix):
+    np.fill_diagonal(distance_matrix, np.inf)
+    return np.unravel_index(np.argmin(distance_matrix, axis=None), distance_matrix.shape)
+
+def hierarchical_clustering(G, n_clusters):
+    n_vertices=G.number_of_nodes()
+    clusters = [[i] for i in range(n_vertices)]
+    distance_matrix = calculate_distance_matrix(G)
+
+    while len(clusters) > n_clusters:
+        i, j = find_closest_clusters(distance_matrix)
+
+        new_cluster = clusters[i] + clusters[j]
+        clusters.append(new_cluster)
+
+        clusters.pop(max(i, j))
+        clusters.pop(min(i, j))
+
+        new_distances = []
+        for k in range(len(distance_matrix)):
+            if k != i and k != j:
+                new_dist = min(distance_matrix[k, i], distance_matrix[k, j])
+                new_distances.append(new_dist)
+        new_distances.append(0.0)  
+
+        distance_matrix = np.delete(distance_matrix, [max(i, j), min(i, j)], axis=0)
+        distance_matrix = np.delete(distance_matrix, [max(i, j), min(i, j)], axis=1)
+        new_distances = np.array(new_distances)
+        distance_matrix = np.vstack((distance_matrix, new_distances[:-1]))
+        distance_matrix = np.column_stack((distance_matrix, new_distances))
+        tau = np.zeros((n_vertices,n_clusters))
+        for l in range(n_vertices):
+            index_l=0
+            for index, sublist in enumerate(clusters):
+                found=False
+                if l in sublist:
+                    index_l=index
+                    found=True
+                if found:
+                    break
+
+            tau[l, clusters[index_l]] = 1
+
+    return tau
+
 def return_priors_pi(X, tau):
     """
     We want to have tau_iq = P(Z_iq = 1 | X)
@@ -109,7 +168,7 @@ def appro_tau(tau, graph_edges, pi, priors, eps = 1e-04, max_iter = 50):
     return tau
 
     
-def main(X, n_clusters, max_iter = 100, method = "spectral"):
+def main(X, n_clusters, max_iter = 100, method = "hierarchical"):
     n_nodes, _ = X.shape
 
     G = nx.from_numpy_array(X)
@@ -117,6 +176,8 @@ def main(X, n_clusters, max_iter = 100, method = "spectral"):
     # Initialize tau 
     if method == "spectral":
         tau = spectral_clustering(G, n_clusters)
+    elif method=='hierarchical':
+        tau=hierarchical_clustering(G, n_clusters)
     elif method == "random":
         tau = np.random.uniform(0, 1, size=(n_nodes, n_clusters))
         tau = tau / tau.sum(axis=1, keepdims=True)

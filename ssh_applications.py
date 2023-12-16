826e8e5d4d3f09ea0790974090c5e7abf83f06aa
ElSacho <sacha.braun99@gmail.com>
2023-12-16 16:55:09 +0100

    results twitter

--- ssh_applications.py
+++ ssh_applications.py
@@ -16,9 +16,6 @@ print("Working on it")
 # Create the directory
 os.makedirs(os.path.join("results",save_name), exist_ok=True)
 
-# Check if the directory was created successfully
-os.path.exists(os.path.join("results",save_name))
-
 print("What data do you want to use ? [metro, political, twitter, miserables] : " , end="")
 data_name = input()
 print("Working on it")
@@ -27,7 +24,7 @@ if data_name == 'metro':
 elif data_name == 'political':
     graph = nx.read_gml("data/political_portrait/political_extraction.gml")
 elif data_name == 'twitter':
-    graph = nx.read("data/congress_network/congress_new_edgelist.txt")
+    graph = nx.read_edgelist("data/congress_network/congress_new_edgelist.txt")
 elif data_name == 'miserables':
     graph = nx.read_gml("data/lesmis/lesmis.gml")
 else:
@@ -37,18 +34,17 @@ else:
 
 
 vd.print_generalitize(graph)
-vd.plot_figure_graph(graph)
+vd.plot_figure_graph(graph, save_path=os.path.join("results",save_name,'graph_information'))
 
 model = mixtureModel(graph, initilisation_method="random")
-tab_clusters = range(2, 40)
+tab_clusters = range(2, 20)
 
 
-model.fit(tab_clusters, save_path=os.path.join("results",'results_'+save_name))
+model.fit(tab_clusters, save_path=os.path.join("results",save_name,'results'))
 
-model.plot_jrx(save_path=os.path.join("results",'jrx_generate'+save_name))
-model.plot_icl(save_path=os.path.join("results",'icl_generate'+save_name))
-model.plot_all_adjency_matrices(save_path=os.path.join("results",'adjency_matrix'+save_name))
-
+model.plot_jrx(save_path=os.path.join("results",save_name,'jrx_generate'))
+model.plot_icl(save_path=os.path.join("results",save_name,'icl_generate'))
+model.plot_all_adjency_matrices(save_path=os.path.join("results",save_name,'adjency_matrix'))
 
 # import numpy as np
 # import matplotlib.pyplot as plt
